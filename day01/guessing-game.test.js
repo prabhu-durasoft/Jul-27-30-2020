edac4790f53fd0e62d962e61b083350fa9c7c069
prabhu-durasoft <iamprabhu@yahoo.com>
2020-07-27 16:40:23 +0530

    Day01

--- day01/guessing-game.test.js
+++ day01/guessing-game.test.js
@@ -1,21 +1,71 @@
 const GuessingGame = require('./guessing-game');
 
+let game = null;
+
+beforeEach(() => {
+    game = new GuessingGame();
+});
+
+afterEach(() => {
+    game = null;
+});
+
 it("should not be null", () => {
-    const game = new GuessingGame();
     expect(game).toBeTruthy();
 });
 
 it("should generate random number between 1 and 100", () => {
-    const game = new GuessingGame();
     expect(game.target).toBeGreaterThan(0);
     expect(game.target).toBeLessThan(100);
 });
 
 it("should generate Aim Higher", () => {
-    const game = new GuessingGame();
     game.target = 77;
     game.play(50);
     const message = game.message;
     expect(message).toBe("Aim Higher");
-
 });
+
+it("should generate Aim Lower", () => {
+    game.target = 77;
+    game.play(80);
+    const message = game.message;
+    expect(message).toBe("Aim Lower");
+});
+
+it("should have 0 attempts in the beginning", () => {
+    expect(game.attempts).toBe(0);
+});
+
+it("should have attempts as 2", () => {
+    game.target = 77;
+    game.play(50);
+    game.play(75);
+    expect(game.attempts).toBe(2);
+});
+
+it("should complete the game", () => {
+    game.target = 77;
+    game.play(50);
+    game.play(77);
+    expect(game.message).toBe("You've got it in 2 attempts");
+});
+
+describe("error cases", () => {
+    it("should not allow to play the game after it's over", () => {
+        game.target = 77;
+        game.play(50);
+        game.play(77);
+        expect(() => {
+            game.play(78);
+        }).toThrow("Game is already over");
+    });
+    
+    it("should validate invalid input", () => {
+        game.target = 77;
+        expect(() => {
+            game.play("32o4973249732");
+        }).toThrow("Invalid input");
+    });
+});
+
