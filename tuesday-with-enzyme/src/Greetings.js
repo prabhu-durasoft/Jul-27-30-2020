6b8846fb4d9b24705dcc31eaafd69ca05a8b4086
prabhu-durasoft <iamprabhu@yahoo.com>
2020-07-28 15:12:55 +0530

    day02

--- tuesday-with-enzyme/src/Greetings.js
+++ tuesday-with-enzyme/src/Greetings.js
@@ -3,9 +3,31 @@ import React from 'react';
 export class Greetings extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            name: null,
+            message: null
+        };
     }
-    
+
+    nameChanged(e) {
+        this.setState({name: e.target.value});
+    }
+
+    buttonClicked() {
+        this.setState({
+            message: `Hello ${this.state.name}`
+        })
+    }
+
+
     render() {
-        return (<div></div>);
+        return (<div>
+            <input type="text" 
+                onChange={this.nameChanged.bind(this)}></input>
+            <br/>
+            <button 
+                onClick={this.buttonClicked.bind(this)}>Click</button>
+            <h1>{this.state.message}</h1>
+        </div>);
     }
 }
--- tuesday-with-enzyme/src/Greetings.test.js
+++ tuesday-with-enzyme/src/Greetings.test.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import Enzyme, { shallow, mount } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
 import { Greetings } from './Greetings';
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+let component;
+beforeEach(() => {
+    component = shallow(<Greetings/>);
+})
+
 it("should render the component", () => {
-    const component = shallow(<Greetings/>);
     expect(component).toBeTruthy();
 });
+
+it("should have a text box to enter the name", () => {
+    const textbox = component.find("input[type='text']");
+    expect(textbox.length).toBe(1);
+});
+
+it("should have a button", () => {
+    const button = component.find('button');
+    expect(button.length).toBe(1);
+});
+
+it("should have a h1", () => {
+    const h1 = component.find('h1');
+    expect(h1.length).toBe(1);
+});
+
+it("should display a message on button click", () => {
+    const textbox = component.find("input[type='text']");
+    textbox.simulate('change', { target: { value: "Sam" }});
+    const button = component.find("button");
+    button.simulate("click");
+    const h1 = component.find("h1");
+    expect(h1.text()).toBe("Hello Sam");    
+});
+
+
